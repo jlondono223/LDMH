1b9dcdd2e84f706ecf823b82546ab5303ab1e9dc
jlondono223 <jl1563@usnh.edu>
2024-03-16 20:16:55 -0400

    Home, Button, and Navbar

---
+++ ldmh-website/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Route, Routes} from 
+'react-router-dom';
+import Home from './components/pages/Home';
+
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+      <Route path='/' element={<Home />} />
+       
+       </Routes>
+    </Router>
+  );
+}
+
+export default App;
